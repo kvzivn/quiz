471ae0a6b97a04e1f757b7c2643af70ae45c0ae5
kvzivn <hello@kevinivan.com>
2020-10-31 16:39:08 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Quiz from './Quiz'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Quiz />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
