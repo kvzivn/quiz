ddc12e577295d6a1b40ba2d79f69e9d49aaac9d3
kvzivn <hello@kevinivan.com>
2020-11-01 02:40:35 +0100

    add quiz context

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,16 +1,25 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx, Grid, Box, Heading, Card } from 'theme-ui'
-import React, { useState, useEffect, useRef} from 'react'
+import { jsx, Grid, Flex, Box, Heading, Card } from 'theme-ui'
+import { useState, useEffect, useRef} from 'react'
+import { useQuizDispatch } from '../context/quiz-context'
 
 export default ({ questions, setStatus }) => {
+  const dispatch = useQuizDispatch()
   const timerRef = useRef(null)
   const [questionNumber, setQuestionNumber] = useState(0)
   const [currentQuestion, setCurrentQuestion] = useState(questions[questionNumber])
-  const [selectedAnswer, setSelectedAnswer] = useState("")
-  const answerTime = 15
+  const answerTime = 5
 
-  const handleChange = e => {
+  const checkAnswer = e => {
+    if (e.target.value === currentQuestion.answer) {
+      dispatch({ type: 'correctAnswer' })
+    }
+
+    gotoNextQuestion()
+  }
+
+  const gotoNextQuestion = () => {
     if (questionNumber >= questions.length - 1) {
       setStatus('finished')
     } else {
@@ -19,28 +28,55 @@ export default ({ questions, setStatus }) => {
   }
 
   useEffect(() => {
+    setCurrentQuestion(questions[questionNumber])
     timerRef.current.style.width = '100%'
 
-    setCurrentQuestion(questions[questionNumber])
+    const countDown = setTimeout(() => {
+      gotoNextQuestion()
+    }, `${answerTime}000`)
+
+    return () => window.clearTimeout(countDown)
   }, [questionNumber])
 
-  return (
-    <React.Fragment>
-      <Box ref={timerRef} sx={{
-        height: '40px',
-        width: 0,
-        backgroundColor: 'primary',
-        transition: `width ${answerTime}s linear`
-      }} />
+  useEffect(() => {
+    const restartTimer = setTimeout(() => {
+      timerRef.current.style.width = '100%'
+    }, 50)
 
-      <Heading sx={{ mt: 10, textAlign: 'center' }}>
+    return () => window.clearTimeout(restartTimer)
+  }, [currentQuestion])
+
+  const Timer = () => (
+    <Box ref={timerRef} sx={{
+      width: 0,
+      height: '8vh',
+      backgroundColor: 'primary',
+      transition: `width ${answerTime}s linear`
+    }} />
+  )
+
+  return (
+    <Flex as="main" sx={{
+      position: 'relative',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      height: '100vh',
+      pt: '6rem'
+    }}>
+      <Heading sx={{
+        maxWidth: [260, 800, '100%'],
+        mx: 'auto',
+        fontSize: ['18px', '20px', '2.5vw'],
+        textAlign: 'center'
+      }}>
         {currentQuestion.question}
       </Heading>
 
-      <Grid columns={[1, 2]} gap={8} sx={{
-        maxWidth: '800px',
-        mt: 8,
+      <Grid columns={[1, 1, 2]} gap={8} sx={{
+        maxWidth: ['800px', '900px', '1000px'],
+        mt: '-4rem',
         mx: 'auto',
+        px: [8, 6],
         fontSize: '20px',
         'label': {
           marginLeft: '.5rem'
@@ -48,19 +84,21 @@ export default ({ questions, setStatus }) => {
       }}>
         {currentQuestion.answers.map((answer, i) => {
           return (
-            <label htmlFor={i} key={i} sx={{
+            <Card as="label" htmlFor={i} key={i} sx={{
               width: '100%',
               boxShadow: '0 20px 30px 0 rgba(79,79,79,.1)',
               backgroundColor: 'white',
-              fontSize: 3,
+              fontSize: ['18px', '20px', '1.75vw'],
               borderRadius: '8px',
-              p: 10,
+              py: '4vw',
+              px: ['2.5vw', '4vw', '5vw'],
               textAlign: 'center',
-              fontSize: 3,
               cursor: 'pointer',
-              transition: 'transform .15s ease-in-out, font-size .15s ease-in-out',
+              transition: 'transform .15s ease-in-out, box-shadow .15s ease-in-out',
+              userSelect: 'none',
               '&:hover': {
-                transform: 'scale(1.05)',
+                transform: 'scale(1.025)',
+                boxShadow: '0 20px 30px 0 rgba(41,41,41,.15)',
               }
             }}>
               <input
@@ -68,7 +106,7 @@ export default ({ questions, setStatus }) => {
                 id={i}
                 value={answer}
                 name="question"
-                onChange={e => handleChange(e)}
+                onClick={e => checkAnswer(e)}
                 sx={{
                   clip: 'rect(0 0 0 0)',
                   clipPath: 'inset(50%)',
@@ -80,10 +118,12 @@ export default ({ questions, setStatus }) => {
                 }}
               />
               {answer}
-            </label>
+            </Card>
           )
         })}
       </Grid>
-    </React.Fragment>
+
+      <Timer sx={{ position: 'absolute', bottom: 0 }} />
+    </Flex>
   )
 }
---
+++ src/context/quiz-context.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const QuizStateContext = React.createContext()
+const QuizDispatchContext = React.createContext()
+
+const quizReducer = (state, action) => {
+  switch (action.type) {
+    case 'correctAnswer':
+      return { correctAnswers: state.correctAnswers + 1 }
+    case 'reset':
+      return { correctAnswers: 0 }
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`)
+  }
+}
+
+const QuizProvider = ({ children }) => {
+  const [state, dispatch] = React.useReducer(quizReducer, { correctAnswers: 0 })
+
+  return (
+    <QuizStateContext.Provider value={state}>
+      <QuizDispatchContext.Provider value={dispatch}>
+        {children}
+      </QuizDispatchContext.Provider>
+    </QuizStateContext.Provider>
+  )
+}
+
+const useQuizState = () => React.useContext(QuizStateContext)
+const useQuizDispatch = () => React.useContext(QuizDispatchContext)
+
+export { QuizProvider, useQuizState, useQuizDispatch }
